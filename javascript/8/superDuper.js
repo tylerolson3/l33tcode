73364154fa17ee89814677efb1d4ff27d8330777
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-05-06 15:16:17 -0500

    add super duper ternary

---
+++ javascript/8/superDuper.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/javascript
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+  return typeof x === "number" ? x * 50 + 6 : "Error";
+}
