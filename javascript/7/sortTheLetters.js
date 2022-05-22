7aa2d586a6e8fa2d1df2ed2c07ef48df12615191
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-05-22 16:57:51 -0500

    add sort letters

---
+++ javascript/7/sortTheLetters.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3/solutions/javascript
+
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+function sortGiftCode(code) {
+  return code.split("").sort().join("");
+}
