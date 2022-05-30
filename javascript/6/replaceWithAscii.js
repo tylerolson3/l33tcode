c6a194d60e9f5c26ed40f13329ff9d858a8adbe4
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-05-30 16:03:09 -0500

    add ascii function

---
+++ javascript/6/replaceWithAscii.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/546f922b54af40e1e90001da/solutions
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map((c) => c.charCodeAt() - 64)
+    .join(" ");
+}
