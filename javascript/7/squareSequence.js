6cdea542b5db4760afb27bc69fbd435a45ea382f
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-07-27 17:20:23 -0500

    add squareSecquence

---
+++ javascript/7/squareSequence.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5546180ca783b6d2d5000062/solutions
+
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+function squares(x, n) {
+  var ret = [];
+  for (var i = 0; i < n; i++) {
+    ret.push(x);
+    x *= x;
+  }
+  return ret;
+}
